1e3d9c4463e363f754df52edadd1749583ff154c
WarwickRen <sorooshesmailiofficial@gmail.com>
2025-01-27 13:29:22 +0330

    Commit number 2:     tokens now save their line number     added parse table.cpp     defined parse tree node data structure     parse tree will now be generated     parse errors added     finished stages 2,3
        minor changes to here and there

---
+++ codes/parse table.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+std::map<std::string, std::string> parsetable() {
+    std::map<std::string, std::string> dictionary;
+    // Parse Table
+    dictionary["Start #include"]        = "S N M";
+    dictionary["S #include"]            = "#include < iostream > S";
+    dictionary["P <"]                   = "epsilon";
+    dictionary["K <"]                   = "epsilon";
+    dictionary["P >"]                   = "epsilon";
+    dictionary["K >"]                   = "epsilon";
+    dictionary["Start using"]           = "S N M";
+    dictionary["S using"]               = "epsilon";
+    dictionary["N using"]               = "using namespace std";
+    dictionary["Z ;"]                   = ";";
+    dictionary["P ;"]                   = "epsilon";
+    dictionary["Assign ;"]              = "epsilon";
+    dictionary["F ;"]                   = "epsilon";
+    dictionary["H ;"]                   = "epsilon";
+    dictionary["Start int"]             = "epsilon";
+    dictionary["S int"]                 = "epsilon";
+    dictionary["N int"]                 = "epsilon";
+    dictionary["M int"]                 = "int main ( ) { T V }";
+    dictionary["T int"]                 = "Id T";
+    dictionary["Id int"]                = "int L";
+    dictionary["P )"]                   = "epsilon";
+    dictionary["T }"]                   = "epsilon";
+    dictionary["V }"]                   = "epsilon";
+    dictionary["T return"]              = "epsilon";
+    dictionary["V return"]              = "return 0 ;";
+    dictionary["T float"]               = "Id T";
+    dictionary["Id float"]              = "float L";
+    dictionary["L IDENTIFIER"]          = "IDENTIFIER Assign Z";
+    dictionary["T IDENTIFIER"]          = "L T";
+    dictionary["Operation IDENTIFIER"]  = "IDENTIFIER P";
+    dictionary["W IDENTIFIER"]          = "IDENTIFIER";
+    dictionary["Expression IDENTIFIER"] = "Operation K Operation";
+    dictionary["C IDENTIFIER"]          = "IDENTIFIER";
+    dictionary["Z ,"]                   = ", IDENTIFIER Assign Z";
+    dictionary["P ,"]                   = "epsilon";
+    dictionary["Assign ,"]              = "epsilon";
+    dictionary["Operation NUMBER"]      = "NUMBER P";
+    dictionary["W NUMBER"]              = "NUMBER";
+    dictionary["Expression NUMBER"]     = "Operation K Operation";
+    dictionary["C NUMBER"]              = "NUMBER";
+    dictionary["P +"]                   = "O W P";
+    dictionary["O +"]                   = "+";
+    dictionary["P -"]                   = "O W P";
+    dictionary["O -"]                   = "-";
+    dictionary["P *"]                   = "O W P";
+    dictionary["O *"]                   = "*";
+    dictionary["Assign ="]              = "= Operation";
+    dictionary["P =="]                  = "epsilon";
+    dictionary["K =="]                  = "==";
+    dictionary["P >="]                  = "epsilon";
+    dictionary["K >="]                  = ">=";
+    dictionary["P <="]                  = "epsilon";
+    dictionary["K <="]                  = "<=";
+    dictionary["P !="]                  = "epsilon";
+    dictionary["K !="]                  = "!=";
+    dictionary["T while"]               = "Loop T";
+    dictionary["Loop while"]            = "while ( Expression ) { T }";
+    dictionary["T cin"]                 = "Input T";
+    dictionary["Input cin"]             = "cin >> IDENTIFIER F";
+    dictionary["F >>"]                  = ">> IDENTIFIER F";
+    dictionary["T cout"]                = "Output T";
+    dictionary["Output cout"]           = "cout << C H";
+    dictionary["H <<"]                  = "<< C H";
+    dictionary["C STRING"]              = "STRING";
+
+    return dictionary;
+}
